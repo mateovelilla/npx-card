4020cf4a91ac3dde4d402ea5fc76b01e7dc8379f
mateovelilla <teovelilla@gmail.com>
2021-09-30 03:02:29 +0000

    refactoring build ssh

--- buildVideo.sh
+++ buildVideo.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-rm -fr frames/*
-rm -fr audios/rickAndMortySong.mp3
+rm -fr ./frames/*
+rm -fr ./audios/rickAndMortySong.mp3
 yes "yes" | ffmpeg -i $1 -vf scale=$2:$3 output.mp4
-yes "yes" | ffmpeg -i output.mp4 -r $4/1 frames/frame%08d.jpg
-yes "yes" | ffmpeg -i output.mp4 audios/rickAndMortySong.mp3
-rm -f output.mp4
+yes "yes" | ffmpeg -i ./output.mp4 -r $4/1 ./frames/frame%08d.jpg
+yes "yes" | ffmpeg -i ./output.mp4 ./audios/rickAndMortySong.mp3
+rm -f ./output.mp4
