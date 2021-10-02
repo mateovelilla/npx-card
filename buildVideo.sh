6f83a0ceadd1697243c68c1fdc9c0fdf971ef6ae
mateovelilla <teovelilla@gmail.com>
2021-10-02 02:43:08 +0000

    Refactored sh

--- buildVideo.sh
+++ buildVideo.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
+
 git clone https://github.com/mateovelilla/npx-card.git
-rm -fr ./npx-card/frames/*
-mkdir ./npx-card/frames
-rm -fr ./npx-card/audios/rickAndMortySong.mp3
-yes "yes" | ffmpeg -i $1 -vf scale=$2:$3 ./npx-card/output.mp4
-yes "yes" | ffmpeg -i ./npx-card/output.mp4 -r $4/1 ./npx-card/frames/frame%08d.jpg
-yes "yes" | ffmpeg -i ./npx-card/output.mp4 ./npx-card/audios/rickAndMortySong.mp3
-rm -f ./npx-card/output.mp4
+#rm -fr ./npx-card/frames/*
+#mkdir ./npx-card/frames
+#rm -fr ./npx-card/audios/rickAndMortySong.mp3
+#yes "yes" | ffmpeg -i $1 -vf scale=$2:$3 ./npx-card/output.mp4
+#yes "yes" | ffmpeg -i ./npx-card/output.mp4 -r $4/1 ./npx-card/frames/frame%08d.jpg
+
+#yes "yes" | ffmpeg -i ./npx-card/output.mp4 ./npx-card/audios/rickAndMortySong.mp3
+#rm -f ./npx-card/output.mp4
