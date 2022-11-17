d6b8d94d48182bc3188124bdc3ab1b123dcc2024
teovelilla <teovelilla@gmail.com>
2022-11-16 21:43:02 -0500

    refactor extension

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -3,6 +3,6 @@ rm -rf ./npx-card
 git clone https://github.com/mateovelilla/npx-card.git
 rm -rf ./npx-card/frames/logos/*
 mkdir ./npx-card/frames/logos
-yes "yes" | ffmpeg -i $1 -vf scale=$2:$3 $4.jpg
+yes "yes" | ffmpeg -i $1 -vf scale=$2:$3 $4
 # yes "yes" | ffmpeg -i ./$4 -r $5/1 ./npx-card/frames/logos/$4
 # rm -rf ./$4
